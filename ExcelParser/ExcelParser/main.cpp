e1b00ca81639a3a83c4df744b1b3448c152a103c
Hyde <ed.cornejo89@gmail.com>
2013-04-11 23:47:04 -0500

    no message

--- ExcelParser/ExcelParser/main.cpp
+++ ExcelParser/ExcelParser/main.cpp
@@ -17,11 +17,36 @@ int main(int argc, const char * argv[])
     
     stack<string> postfija;
     list<string> salida;
-    string entrada="-4*(-5-2)/(-1*2=x-(-3))";
-   
+    string entrada="-467*(-5-2)/(-1*2=x-(-3))";
+    string bufferNum="";
     for (int a=0; a<entrada.size(); ++a) {
         if (entrada[a]>='0' && entrada[a]<='9'){
+            
+            //for (int b=0;(a+b)<=entrada.size()&&(entrada[a+b]>='0' && entrada[a+b]<='9');++b){
+              //  buffer+=entrada[a+b];
+            //}
+            bufferNum+=entrada[a];
+                cout<<bufferNum<<" Es un numero"<<endl;
+            
+        }
         
+        else if ((entrada[a]>='a' && entrada[a]<='z')|| (entrada[a]>='A' && entrada[a]<='Z')){
+            cout<<entrada[a]<<" Es un variable"<<endl;
+            bufferNum="";
+        }
+        
+        else if (entrada[a]=='('){
+            cout<<entrada[a]<<" parentesis izquierdo"<<endl;
+            bufferNum="";
+        }
+        else if (entrada[a]==')'){
+            cout<<entrada[a]<<" parentesis derecho"<<endl;
+            bufferNum="";
+        }
+        
+        else {
+                cout<<entrada[a]<<" operador"<<endl;
+            bufferNum="";
         }
         
     }
