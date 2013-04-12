c8fa64e1babb08749911fe363c08a736ae29b11a
Hyde <ed.cornejo89@gmail.com>
2013-04-11 22:34:57 -0500

    no message

--- ExcelParser/ExcelParser/main.cpp
+++ ExcelParser/ExcelParser/main.cpp
@@ -2,17 +2,24 @@
 //  main.cpp
 //  ExcelParser
 //
-//  Created by Edwin Cornejo Mayta on 11/04/13.
+//  Created by Alison Torres Aguilar - Edwin Cornejo Mayta on 11/04/13.
 //  Copyright (c) 2013 CS-UNSA. All rights reserved.
 //
 
 #include <iostream>
+#include <stack>
+using namespace std;
+
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    stack<string> S;
+    S.push("86");
+    S.push("34");
+    S.push("22");
+    cout<<S.top();
+    
     return 0;
 }
 
